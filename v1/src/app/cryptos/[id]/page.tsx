2790bd0876ddd7f7db073a73a23cd8cc113e2c13
Kevin Zapata <148372120+zaokdev@users.noreply.github.com>
2024-12-16 19:33:42 -0600

    Delete v1 directory

--- v1/src/app/cryptos/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import CryptoDetails from '@/components/CryptoDetails'
-import React  from 'react'
-
-export default function Crypto() {
-
-  return (
-    <CryptoDetails />
-  )
-}
--- v1/src/components/utils/Back.tsx
+++
@@ -1,9 +0,0 @@
-import { StepBack } from 'lucide-react'
-import Link from 'next/link'
-import React from 'react'
-
-export default function Back() {
-  return (
-    <Link href="/"><StepBack></StepBack></Link>
-  )
-}
