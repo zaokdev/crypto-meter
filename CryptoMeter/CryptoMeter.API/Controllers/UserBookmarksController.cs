c787fffdcd32e5924d243fff9f55a8250e496d7d
zaokdev <kevinzaok@outlook.com>
2025-01-04 22:08:57 -0600

    Finished backend

--- CryptoMeter/CryptoMeter.API/Controllers/UserBookmarksController.cs
+++ CryptoMeter/CryptoMeter.API/Controllers/UserBookmarksController.cs
@@ -1,6 +1,8 @@
 ﻿using CryptoMeter.Data;
+using CryptoMeter.Shared;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,10 +14,13 @@ namespace CryptoMeter.API.Controllers
     public class UserBookmarksController : ControllerBase
     {
         private readonly APICryptoMeterContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public UserBookmarksController(APICryptoMeterContext context)
+
+        public UserBookmarksController(APICryptoMeterContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         [HttpGet("all")]
@@ -30,6 +35,55 @@ namespace CryptoMeter.API.Controllers
         {
             var UserBookmarks = await _context.UserCryptoRelations.Where(c => c.UserId == userId).Select(c => c.CryptoId).ToListAsync();
             return Ok(UserBookmarks);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddBookmark([FromQuery] string userId, int cryptoId)
+        {
+            var UserExists = await _userManager.FindByIdAsync(userId);
+
+            if (UserExists == null) {
+                return BadRequest("User does not exists");
+            }
+
+            var relationAlreadyExists = await _context.UserCryptoRelations.FirstOrDefaultAsync(c => c.UserId == userId && c.CryptoId == cryptoId);
+
+            if (relationAlreadyExists != null)
+            {
+                return BadRequest("Already bookmarked. Delete it first.");
+            }
+
+            var newRelation = new UserCryptoRelation() {
+                UserId = userId,
+                CryptoId = cryptoId
+
+            };
+            await _context.UserCryptoRelations.AddAsync(newRelation);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteBookmark([FromQuery] string userId, int cryptoId)
+        {
+            var UserExists = await _userManager.FindByIdAsync(userId);
+
+            if (UserExists == null)
+            {
+                return BadRequest("User does not exists.");
+            }
+
+            var relationAlreadyExists = await _context.UserCryptoRelations.FirstOrDefaultAsync(c => c.UserId == userId && c.CryptoId == cryptoId);
+
+            if(relationAlreadyExists == null)
+            {
+                return BadRequest("User does not have this bookmark.");
+            }
+
+            _context.UserCryptoRelations.Remove(relationAlreadyExists);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
