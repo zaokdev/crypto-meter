d621c5244b853fa369a9c0eaf859c58468217af9
zaokdev <kevinzaok@outlook.com>
2025-01-04 23:05:21 -0600

    Production ready

--- CryptoMeter/CryptoMeter.API/obj/Debug/net8.0/CryptoMeter.API.AssemblyInfo.cs
+++ CryptoMeter/CryptoMeter.API/obj/Debug/net8.0/CryptoMeter.API.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CryptoMeter.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94075647015983a859e6c18a7a266139d56f124c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c787fffdcd32e5924d243fff9f55a8250e496d7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CryptoMeter.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CryptoMeter.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CryptoMeter/CryptoMeter.Shared/obj/Debug/net8.0/CryptoMeter.Shared.AssemblyInfo.cs
+++ CryptoMeter/CryptoMeter.Shared/obj/Debug/net8.0/CryptoMeter.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CryptoMeter.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21327d1f92d8adf927df2db70940452ec54c74d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c787fffdcd32e5924d243fff9f55a8250e496d7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CryptoMeter.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CryptoMeter.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
